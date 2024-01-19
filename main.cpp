c14a47ae35fe5593199e1cf562b7575d3d04f532
Darius Ronkowitz <dronkowitz@yahoo.com>
2024-01-19 15:22:39 -0500

    `Create Cpp Quiz Program`
    Learn about what C++ is, this program is used for learning C++

---
+++ main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+int main() {
+  std::cout << "Enter a number: ";
+  int virtual {};
+  std::cin >> virtual;
+  std::cout << "You entered " << virtual << '\n';
+  int value;
+  [x] int value;
+  [x] int VALUE;
+  int myVariableName(5);
+  int my_variable_name(5);
+  int myVariableName2(5);
+  bool isOn(true);
+  bool isOff(false);
+  std::cout << sizeof(int) << '\n';
+  
+  return 0;
+}
